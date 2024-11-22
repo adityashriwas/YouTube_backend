1cb2028d5df2cdec154bea19bc71c8b65bb567d7
Aditya Shriwas <adityashriwas18@gmail.com>
2024-11-22 06:39:10 -0800

    setup API responces, error and handler

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ dotenv.config({
     path: "/.env"
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.error("MONGODB Connection failed!!! ", error);
+    process.exit(1);
+})
 
 
 
