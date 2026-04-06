9279e5e4eeceac75bc7e2b1fe372198e46ba5c95
Aditya Shriwas <aditya.shriwas@sofmen.com>
2026-04-06 13:44:50 +0530

    Setup nativewind and resolved error

--- VidTube/.eslintrc.js
+++ VidTube/.eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      files: [
+        '*.config.js',
+        '.eslintrc.js',
+      ],
+      parserOptions: {
+        requireConfigFile: false,
+      },
+    },
+  ],
 };
